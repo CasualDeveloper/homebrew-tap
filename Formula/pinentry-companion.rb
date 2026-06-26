e83250aff5add4073cb8ab775b62350bf88d6f57
CasualDeveloper <10153929+CasualDeveloper@users.noreply.github.com>
2026-06-27 00:39:00 +0800

    Update pinentry-companion to 0.1.2

--- Formula/pinentry-companion.rb
+++ Formula/pinentry-companion.rb
@@ -4,7 +4,7 @@
 class PinentryCompanion < Formula
   desc "Native macOS GPG pinentry with Apple Watch, Touch ID, and Keychain support"
   homepage "https://github.com/CasualDeveloper/pinentry-companion"
-  version "0.1.1"
+  version "0.1.2"
   license "Apache-2.0"
   head "https://github.com/CasualDeveloper/pinentry-companion.git", branch: "main"
 
@@ -15,13 +15,13 @@ class PinentryCompanion < Formula
 
   on_macos do
     on_arm do
-      url "https://github.com/CasualDeveloper/pinentry-companion/releases/download/v0.1.1/pinentry-companion-v0.1.1-arm64.tar.gz"
-      sha256 "5eb0df529c8402944c84a0b10fa4eb0345c90e78cd33b342b6a08ac46cef58b6"
+      url "https://github.com/CasualDeveloper/pinentry-companion/releases/download/v0.1.2/pinentry-companion-v0.1.2-arm64.tar.gz"
+      sha256 "ad974d75438bd08a81514d710cfcee7dbfe404933788431841968deb84a2cfc7"
     end
 
     on_intel do
-      url "https://github.com/CasualDeveloper/pinentry-companion/releases/download/v0.1.1/pinentry-companion-v0.1.1-x86_64.tar.gz"
-      sha256 "7b3c589a0ffc12b0d81a2e44f6455ae6e39c2d98d210017e4165d128e0a3fd85"
+      url "https://github.com/CasualDeveloper/pinentry-companion/releases/download/v0.1.2/pinentry-companion-v0.1.2-x86_64.tar.gz"
+      sha256 "4b4c3f0a5706fe95b1a81b556f655a2cffbeb31841cf9796d2b4bab3250b70a9"
     end
   end
 
