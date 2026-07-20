d65443debe6c9554ed16b49e60656a30042f9557
CasualDeveloper <10153929+CasualDeveloper@users.noreply.github.com>
2026-07-21 05:48:32 +0800

    authcompanion 0.1.1

--- Formula/authcompanion.rb
+++ Formula/authcompanion.rb
@@ -4,8 +4,8 @@
 class Authcompanion < Formula
   desc "Set up Touch ID and Apple Watch authentication for GPG signing and sudo"
   homepage "https://github.com/CasualDeveloper/AuthCompanion"
-  url "https://github.com/CasualDeveloper/AuthCompanion/releases/download/v0.1.0/authcompanion-0.1.0.tar.gz"
-  sha256 "1011838a3e8b5812500bfdb6a50ecef96173a50e24c4c372f8a04fc98e851ca0"
+  url "https://github.com/CasualDeveloper/AuthCompanion/releases/download/v0.1.1/authcompanion-0.1.1.tar.gz"
+  sha256 "3597dd380e1c88ba35b6af6d3e37af3c069d6443c70941e49efbd5660b0fc085"
   license "Apache-2.0"
 
   depends_on macos: :sonoma
@@ -20,12 +20,15 @@ class Authcompanion < Formula
     <<~EOS
       Review the combined GPG and PAM plan, then apply it explicitly:
         #{opt_bin}/authcompanion plan
+        sudo -v
         #{opt_bin}/authcompanion setup --yes
 
       Check both components:
+        sudo -v
         #{opt_bin}/authcompanion doctor
 
       Before uninstalling, restore both components:
+        sudo -v
         #{opt_bin}/authcompanion restore --yes
     EOS
   end
@@ -33,7 +36,7 @@ class Authcompanion < Formula
   test do
     companion = bin/"authcompanion"
 
-    assert_equal "authcompanion 0.1.0", shell_output("#{companion} --version").strip
+    assert_equal "authcompanion 0.1.1", shell_output("#{companion} --version").strip
     assert_match "authcompanion setup --yes", shell_output("#{companion} --help")
     assert_match "requires explicit --yes confirmation", shell_output("#{companion} setup 2>&1", 2)
   end
